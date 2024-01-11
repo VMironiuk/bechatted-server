900bc44e101a72b9bda3b21f1b787eda46da894d
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-01-11 19:34:04 +0200

    Add 'hello' request handler

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use actix_web;
+use actix_web::{get, HttpResponse, Responder};
 
 #[actix_web::main]
 async fn main() {
     println!("Hello, world!");
 }
 
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Bonjour!")
+}
