a389de9ac5553b7af13b95ad3320c3aad81dc1f6
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-01-12 19:16:42 +0200

    Add "/api/v1" aplication scope

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main()  -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(
+                web::scope("/api/v1")
+                    .service(hello)
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
