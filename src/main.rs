a81e9cea126b24701db44f1ba47b75f31661403c
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-01-13 12:13:08 +0200

    Add 'AccountRepository' to store new accounts

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use actix_web::{post, web, App, HttpServer};
 use serde::Deserialize;
+use std::sync::Mutex;
 
 #[actix_web::main]
 async fn main()  -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let account_repository = web::Data::new(AccountRepository {
+        accounts: Mutex::new(Vec::new())
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(account_repository.clone())
             .service(
                 web::scope("/api/v1")
                     .service(register_account)
@@ -21,7 +27,22 @@ struct NewAccountInfo {
     password: String
 }
 
+#[derive(Debug)]
+struct Account {
+    _email: String,
+    _password: String
+}
+
+struct AccountRepository {
+    accounts: Mutex<Vec<Account>>
+}
+
 #[post("/account/register")]
-async fn register_account(new_account_info: web::Json<NewAccountInfo>) -> String {
-    format!("New Account: {}, {}", new_account_info.email, new_account_info.password)
+async fn register_account(new_account_info: web::Json<NewAccountInfo>, data: web::Data<AccountRepository>) -> String {
+    let mut accounts = data.accounts.lock().unwrap();
+    accounts.push(Account {
+        _email: new_account_info.email.clone(),
+        _password: new_account_info.password.clone()
+    });
+    format!("Accounts Number: {}\nAccounts, {:#?}", accounts.len(), accounts)
 }
